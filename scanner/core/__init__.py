3fae99bc532d7aacea11714572b03a0f824de236
spy15007-arch <spy.15007@gmail.com>
2026-08-23 09:39:37 +0530

    Add docstring to __init__.py
    Add module docstring for core package.

--- scanner/core/__init__.py
+++ scanner/core/__init__.py
@@ -1,1 +1,11 @@
+"""
+Trading OS v12
+Core package
+"""
 
+from .indicators import *
+from .downloader import *
+from .scoring import *
+from .market import *
+from .risk import *
+from .utils import *
